0cbec8e23b3138ef5066a98e617d2c3dddef4cc8
Reece Emero <reece.emero98@gmail.com>
2019-08-19 01:03:36 -0400

    Checks command

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+const config = require('./config.json')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+
+const sentimentScore = 0
+
+client.once('ready', () => {
+	console.log('Sentiment Bot has connected!')
+})
+
+client.on('message', message => {
+	if (message.content.startsWith('!sentiment')) {
+		message.channel.send(`Sentiment Level: \`${sentimentScore}\``)
+	}
+	else if (message.author.id !== client.user.id) {
+		message.channel.send(`Message to be analyzed: ${message}`)
+	}
+})
+
+client.login(config.token)
