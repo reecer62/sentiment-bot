2fd4bda4608bcd3051e26588c0524c5d2ee94bcd
Reece Emero <reece.emero98@gmail.com>
2019-08-19 02:32:04 -0400

    Added sentiment calculation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const config = require('./config.json')
+const Sentiment = require('sentiment')
+const sentiment = new Sentiment()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
 
-const sentimentScore = 0
+let sumScore = 0
+let numMessages = 0
+let avgScore = 0
 
 client.once('ready', () => {
 	console.log('Sentiment Bot has connected!')
@@ -11,10 +15,14 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if (message.content.startsWith('!sentiment')) {
-		message.channel.send(`Sentiment Level: \`${sentimentScore}\``)
+		message.channel.send(`Current sentiment level: \`${avgScore}\``)
 	}
 	else if (message.author.id !== client.user.id) {
-		message.channel.send(`Message to be analyzed: ${message.content}`)
+		const sentimentScore = sentiment.analyze(message.content).score
+		numMessages++
+		sumScore += sentimentScore
+		avgScore = sumScore / numMessages
+		message.channel.send(`Message to be analyzed: ${message.content}\nMessage score: ${sentimentScore}`)
 	}
 })
 
