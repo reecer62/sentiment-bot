23c9cb5127ee10a648e829bcf8ebdaa8dd761de9
Reece Emero <reece.emero98@gmail.com>
2019-08-19 11:20:40 -0400

    Added simple reaction to messages

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const config = require('./config.json')
+const nicejob = require('nicejob')
 const Sentiment = require('sentiment')
 const sentiment = new Sentiment()
 const Discord = require('discord.js')
 const client = new Discord.Client()
-
 
 let sumScore = 0
 let numMessages = 0
@@ -22,7 +22,13 @@ client.on('message', message => {
 		numMessages++
 		sumScore += sentimentScore
 		avgScore = sumScore / numMessages
-		message.channel.send(`Message to be analyzed: ${message.content}\nMessage score: ${sentimentScore}`)
+		if (sentimentScore >= 0) {
+			message.channel.send(nicejob())
+		}
+		else {
+			message.channel.send(nicejob.not())
+		}
+		// message.channel.send(`Message to be analyzed: ${message.content}\nMessage score: ${sentimentScore}`)
 	}
 })
 
