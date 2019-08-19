96b45d6895e44f066603c13a9fc3d3c0206457c2
Reece Emero <reece.emero98@gmail.com>
2019-08-19 11:48:30 -0400

    Add ability to get score from 10 most recent msgs

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const client = new Discord.Client()
 let sumScore = 0
 let numMessages = 0
 let avgScore = 0
+const messageStack = []
 
 client.once('ready', () => {
 	console.log('Sentiment Bot has connected!')
@@ -15,10 +16,24 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if (message.content.startsWith('!sentiment')) {
-		message.channel.send(`Current sentiment level: \`${avgScore}\``)
+		const args = message.content.slice(1).split(/ +/)
+		if (args.length > 1 && !isNaN(parseInt(args[1]))) {
+			const messageId = parseInt(args[1])
+			messageId > messageStack.length ? message.channel.send(`Can't access message ${messageId}`) : message.channel.send(`Score for message ${messageId}: ${messageStack[messageId - 1].score}`)
+		}
+		else {
+			message.channel.send(`Current sentiment level: \`${avgScore}\``)
+		}
 	}
 	else if (message.author.id !== client.user.id) {
 		const sentimentScore = sentiment.analyze(message.content).score
+		if (messageStack.length === 10) {
+			messageStack.pop()
+		}
+		messageStack.unshift({
+			'score': sentimentScore,
+			'message': message.content,
+		})
 		numMessages++
 		sumScore += sentimentScore
 		avgScore = sumScore / numMessages
